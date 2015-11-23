acfa3344977e48027ae8735dcf49fe9556be636d
Jason <hinchy@gmail.com>
2015-11-23 14:06:43 +1100

    Removed test files and cleaned up dir

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_start();
-if (isset($_COOKIE['test'])) {
-    echo 'cookie is fine<br>';
-    print_r($_COOKIE);
-} else {
-    setcookie('test', 'cookie test content', time()+3600);  /* expire in 1 hour */
-    echo 'Trying to set cookie. Reload page plz';
-}
